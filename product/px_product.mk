2d92dfed48180dbffc6da01dfc0dcb99a954c27f
noname55804 <marlin2201@gmail.com>
2015-05-31 15:00:57 +0200

    sabermod vendor: make ready for ProjectX
    add makefiles for px_m8
    Add some variables for inline kernel building
    
    Signed-off-by: noname55804 <marlin2201@gmail.com>
    
    Conflicts:
    	device/sm_devices.mk
    	product/sm_products.mk
    
    Change-Id: I11d7e259ca08c250005d3319a9c7a32dc1f3f3b0

--- product/px_product.mk
+++ product/px_product.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 The SaberMod Project
+# Copyright (C) 2015 ProjectX
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,18 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-ifdef SM_AND_NAME
-$(info   TARGET_SABERMOD_ANDROID_GCC_VERSION=$(SM_AND_NAME))
-endif
-ifdef SM_KERNEL_NAME
-$(info   TARGET_SABERMOD_KERNEL_GCC_VERSION=$(SM_KERNEL_NAME))
-endif
-ifneq ($(strip $(USE_KERNEL_OPTIMIZATIONS)),true)
-$(info   KERNEL_OPTIMIZATIONS=true)
-else
-$(info   KERNEL_OPTIMIZATIONS=false)
-endif
-ifdef GCC_OPTIMIZATION_LEVELS
-$(info   OPTIMIZATION_OPTIONS=$(GCC_OPTIMIZATION_LEVELS))
-endif
+# Set Optimzation Version
+
+OPT_VERSION := 1.0.0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.opt.version=$(OPT_VERSION)
+
+
+#Disable some modules
+
+LOCAL_ARM_COMPILERS_WHITELIST := \
+    libpng 
