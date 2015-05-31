cf400bfdc248819d5c1d07b1c47b8d2743875d62
noname55804 <marlin2201@gmail.com>
2015-05-31 15:00:57 +0200

    sabermod: add px_m8
    Change-Id: I22c1e13751e672f435a9b8965fae3a549e7d57e5
    Signed-off-by: noname55804 <marlin2201@gmail.com>

--- product/sm_products.mk
+++ product/sm_products.mk
@@ -32,7 +32,7 @@ ifneq ($(filter aosp% boostpop% pa%,$(TARGET_PRODUCT)),)
   include $(SM_VENDOR)/product/aosp_product.mk
 endif
 
-ifneq ($(filter slim% aicp% cm%,$(TARGET_PRODUCT)),)
+ifneq ($(filter slim% aicp% cm% px%,$(TARGET_PRODUCT)),)
   TARGET_BASE_ROM := cm
   include $(SM_VENDOR)/product/cm_product.mk
 endif
